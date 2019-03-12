1a0ffa3a5ab5284d9dddb4b618d574a968b44cc9
BaoTNM <BaoTNguyenM@gmail.com>
2019-03-12 21:17:23 +0100

    changing styles.xml and manifest to remove actionbar in fragment

--- app/src/main/java/com/example/btnm/drinkwater2/tabFragments/HomeFragment.java
+++ app/src/main/java/com/example/btnm/drinkwater2/tabFragments/HomeFragment.java
@@ -9,6 +9,7 @@ import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,7 +36,6 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_home,container, false);
-
 
 
         btnTest = (Button) view.findViewById(R.id.btnTest);
