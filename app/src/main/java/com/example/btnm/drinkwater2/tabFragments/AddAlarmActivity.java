791b5084598dedc907a3b40ce826b31ee9b60505
BaoTNM <BaoTNguyenM@gmail.com>
2019-03-27 21:45:30 +0100

    can save the image pos from the spinner

--- app/src/main/java/com/example/btnm/drinkwater2/tabFragments/AddAlarmActivity.java
+++ app/src/main/java/com/example/btnm/drinkwater2/tabFragments/AddAlarmActivity.java
@@ -1,13 +1,16 @@
 package com.example.btnm.drinkwater2.tabFragments;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.btnm.drinkwater2.R;
 import com.example.btnm.drinkwater2.SimpleImageArrayAdapter;
@@ -19,10 +22,11 @@ public class AddAlarmActivity extends AppCompatActivity {
 
     private static Integer[] imageIconData =  {
             R.drawable.ic_android, R.drawable.ic_audio, R.drawable.ic_sun, R.drawable.ic_add, R.drawable.ic_small_waterdrop_blue,R.drawable.ic_small_waterdrop_red };
-    private String[] imageNameData = {
-            "android","audio","sun","add" };
+    private String[] imageNameData = { "android","audio","sun","add" };
 
     private Spinner spinner;
+    private int alarmDuration;
+    private Button saveBtn, cancelBtn;
 
     private ArrayList<HashMap<String, Object>> spinnerList;
     private HashMap<String, Object> hashMap;
@@ -39,12 +43,41 @@ public class AddAlarmActivity extends AppCompatActivity {
 //        setSupportActionBar(addAlarmToolbar);
 
 //        addItemOnSpinner();
-
 //        initializeImageList();
+
         Spinner spinner2 = (Spinner) findViewById(R.id.iconChooseSpinner);
 //        CustomSpinnerAdapter adapter = new CustomSpinnerAdapter();
         SimpleImageArrayAdapter simpleImageArrayAdapter = new SimpleImageArrayAdapter(getBaseContext(), imageIconData);
         spinner2.setAdapter(simpleImageArrayAdapter);
+
+        ImageView testIcon = (ImageView) spinner2.getSelectedItem();
+        ImageView tempImage = (ImageView) findViewById(R.id.testImage);
+
+
+        EditText inputAlarm = (EditText)  findViewById(R.id.inputAlarmTime);
+        saveBtn = (Button) findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener((e) -> {
+//            String test = inputAlarm.getText().toString();
+
+            if ( inputAlarm.getText().toString().trim().isEmpty() || Integer.parseInt(inputAlarm.getText().toString()) > 1 ) {
+                alarmDuration = (int) Integer.valueOf(inputAlarm.getText().toString() );
+                Toast.makeText(getBaseContext(), "Test editText for now: "+alarmDuration + "test2 : "+ inputAlarm.getText().toString(),Toast.LENGTH_SHORT).show();
+
+//                tempImage.setImageResource((Integer) spinner2.getSelectedItem());
+                tempImage.setImageResource(imageIconData[spinner2.getSelectedItemPosition()] );
+
+//                testIconCheck.setImageResource(spinner2.getSelectedView());
+//                testIconCheck.setImageResource(R.drawable.ic_audio);
+
+//                testIconCheck.setImageResource(imageIconData[spinner2.getSelectedItemPosition()] );
+            }
+        });
+        cancelBtn = (Button) findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener( (e) -> {
+            finish();
+        });
+
+
 
     }
 
